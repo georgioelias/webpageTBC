cb459eee86099568d8088b79f740b796c61af70f
GeorgioElias <115529772+georgioelias@users.noreply.github.com>
2024-09-12 10:57:24 +0300

    Update webtbc1.py

--- webtbc1.py
+++ webtbc1.py
@@ -27,29 +27,24 @@ st.markdown(
         font-family: 'Roboto', sans-serif;
     }
     .main-title {
-        color: #FF4B4B;
+        color: #333333;
         font-family: 'Roboto', sans-serif;
         font-size: 36px;
         text-align: center;
         margin-top: 20px;
     }
     .sub-title {
-        color: #4BFF94;
+        color: #007BFF;
         font-family: 'Roboto', sans-serif;
         text-align: center;
         margin-bottom: 20px;
     }
     .description {
-        color: #FFDA4B;
+        color: #555555;
         font-family: 'Roboto', sans-serif;
         text-align: center;
         font-size: 16px;
         margin-bottom: 20px;
-    }
-    .input-text {
-        color: #4BFFEB;
-        font-size: 18px;
-        font-family: 'Roboto', sans-serif;
     }
     .form-input {
         margin: 10px 0;
@@ -59,11 +54,15 @@ st.markdown(
         font-size: 14px;
     }
     .footer {
-        color: #FF6B6B;
+        color: #333333;
         font-family: 'Roboto', sans-serif;
         text-align: center;
         font-size: 18px;
         margin-top: 30px;
+    }
+    .icon-small {
+        width: 20px;
+        height: 20px;
     }
     @media only screen and (max-width: 600px) {
         .main-title {
@@ -94,26 +93,35 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Instagram Links with Icons
+# Instagram Links with smaller icons
 st.markdown("<h3 class='sub-title'>Check out our Instagram:</h3>", unsafe_allow_html=True)
-st.write("[![Instagram](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png)](https://instagram.com/georgioelias) Follow @georgioelias")
-st.write("[![Instagram](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png)](https://instagram.com/jp_nassif) Follow @jp_nassif")
+st.write(f"""
+    <a href="https://instagram.com/georgioelias" target="_blank">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" class="icon-small"> Follow @georgioelias
+    </a>
+""", unsafe_allow_html=True)
+
+st.write(f"""
+    <a href="https://instagram.com/jp_nassif" target="_blank">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" class="icon-small"> Follow @jp_nassif
+    </a>
+""", unsafe_allow_html=True)
 
 # User Information Form
 st.markdown("<h3 class='sub-title'>Join The Bite Club</h3>", unsafe_allow_html=True)
 
 with st.form(key='user_info_form'):
-    name = st.text_input("Full Name", help="Enter your name", key="name", class_="form-input")
-    email = st.text_input("Email Address", help="Enter your email", key="email", class_="form-input")
-    instagram_handle = st.text_input("Instagram Handle", help="Enter your Instagram username", key="instagram", class_="form-input")
-    favorite_food = st.text_input("Favorite Food", help="What's your favorite dish?", key="favorite_food", class_="form-input")
+    name = st.text_input("Full Name", help="Enter your name")
+    email = st.text_input("Email Address", help="Enter your email")
+    instagram_handle = st.text_input("Instagram Handle", help="Enter your Instagram username")
+    favorite_food = st.text_input("Favorite Food", help="What's your favorite dish?")
     
     st.markdown("### Club Entry Questions")
-    q1 = st.text_input("Why are you worthy of entering 'thebiteclub'?", key="q1", class_="form-input")
-    q2 = st.text_input("What’s the most ridiculous food combination that you secretly love?", key="q2", class_="form-input")
-    q3 = st.text_input("If 'thebiteclub' had a mascot, what would it be and why?", key="q3", class_="form-input")
-    q4 = st.text_input("What secret food talent makes you stand out from the rest?", key="q4", class_="form-input")
-    q5 = st.text_input("Which food item best represents your personality, and how would it make the club better?", key="q5", class_="form-input")
+    q1 = st.text_input("Why are you worthy of entering 'thebiteclub'?")
+    q2 = st.text_input("What’s the most ridiculous food combination that you secretly love?")
+    q3 = st.text_input("If 'thebiteclub' had a mascot, what would it be and why?")
+    q4 = st.text_input("What secret food talent makes you stand out from the rest?")
+    q5 = st.text_input("Which food item best represents your personality, and how would it make the club better?")
     
     submit_button = st.form_submit_button(label='Submit')
 
@@ -124,12 +132,12 @@ with st.form(key='user_info_form'):
         else:
             st.error("Please fill in all fields.")
 
-# Add a footer with a cool call-to-action
+# Add a footer with a call-to-action
 st.markdown(
     """
     <style>
     .footer {
-        color: #FF6B6B;
+        color: #333333;
         font-family: 'Roboto', sans-serif;
         text-align: center;
         font-size: 18px;
